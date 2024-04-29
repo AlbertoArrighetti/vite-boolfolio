b4d24028dc23c1a5a400ac310a6da44b3dff0497
alan_omam <omamalan@hotmail.it>
2024-04-29 18:40:34 +0200

    feat item

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,37 @@
+<script>
+export default{
+      name:"ProjectCard",
+
+      props:{
+        comName: String,
+        comImage: String
+      },
+
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+
+<div class="">
+        <!--Indico dove in uno specifico figlio verranno usati le variabili props-->
+        <img  :src="comImage" alt="">
+        <span>{{ comName }}</span>
+
+
+
+ 
+      
+</div>
+
+</template>
+
+<style lang="scss">
+
+
+
+</style>
