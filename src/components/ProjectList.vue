d38d526d0d2edfa66951453fd50481e7adbe485d
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-04-29 18:40:28 +0200

    wip: list

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,25 @@
+<script>
+import ProjectCard from './ProjectCard.vue';
+
+export default {
+    name: 'ProjectCard',
+
+    components: {
+        ProjectCard,
+    },
+}
+</script>
+
+<template>
+    <div>
+        <ul>
+            <ProjectCard v-for="project in projects">
+
+            </ProjectCard>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
