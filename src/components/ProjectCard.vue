923b5087eccb8d5aa170070097548dc4fb8cdb75
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-04-30 16:31:31 +0200

    fix: link to show

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -16,12 +16,13 @@ export default {
 
 <template>
 
-<div class="">
+<div>
        
-  <li class="py-3">
+  <li class="py-3 d-flex gap-4 ">
      <span>{{ card.title }}</span>
-     
-   <router-link :to="{name: 'single-post', params: {id: post.id}}"  class="btn btn-outline-info btn-outline">Mostra</router-link>
+     <router-link  :to="{name: 'single-project', params: {id: card.id}}" class="btn btn-outline-info btn-outline">
+        Mostra
+    </router-link>
   </li>
     
 </div>
