42cf4809331fdd0ba1878d3993ced6302c443bb3
alan_omam <omamalan@hotmail.it>
2024-04-30 17:45:55 +0200

    modificato stile singolo progetto

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -40,18 +40,33 @@ export default {
 
 <template>
 
-    <div v-if="project" class="container py-5 fs-2">
-        <ul>
+
+<div v-if="project" class="container py-5 fs-2">
+    <h1 class="py-4">PAGINA DEL POST SINGOLO </h1>
+<!----->
+<div class="card bg-info"  style="width: 70rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <ul class="nav flex-column">
             <li><strong>Project title : </strong>{{ project.title }}</li>
             <li><strong>Project description : </strong>{{project.description}}</li>
-            <li><strong>Project type : </strong>{{project.type.name}}</li>
             <li><strong>Project technologies : </strong>
                 <span v-for=" technology in project.technologies " class="badge rounded-pill text-bg-success">
                     {{ technology.name}}
                 </span>
             </li>
         </ul>
+  </div>
+</div>
+<!----->
+
     </div>
+
+  
+
+  
+
+  
 
     <div v-else>
         <div class="spinner-border" role="status">
